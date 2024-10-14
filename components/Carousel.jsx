d0e0817890daf074c4a9d4fa6d0509d6a1882a6f
abdulrahmansoyooye <abdulrahmansoyooye@gmail.com>
2024-10-14 10:24:06 +0100

    First Commit

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -63,7 +63,7 @@ const Carousel = () => {
     visible: { opacity: 1, y: 0 },
   };
   return (
-    <div className="relative flex flex-col  w-[100vw]  gap-[9rem] justify-center section  mt-[7rem] ">
+    <div className="relative flex flex-col h-[100vh] w-[100vw]  gap-[9rem] justify-center section  mt-[7rem] ">
       <div className="flex gap-[1rem] justify-center">
         {carouselItem.map(
           ({ title, content, buttonText }, index) =>
@@ -73,13 +73,13 @@ const Carousel = () => {
                 animate="visible"
                 variants={variants}
                 transition={{ duration: 0.5 }}
-                className="flex flex-col  gap-[5rem]  carousel-item justify-center  text-center "
+                className="flex flex-col  gap-[5rem] px-[4rem] break-words carousel-item justify-center  text-center "
                 key={`${index}-${title}`}
               >
                 <div className="flex flex-col gap-[1rem] ">
-                  <div className="text-[2rem] max-lg:text-[1.5rem] dark-text serif">
+                  <p className="text-[2rem] max-lg:text-[1.5rem] dark-text serif">
                    {title}
-                  </div>
+                  </p>
 
                   <motion.p className="font-[400] dark-text text-[0.9rem] sm:text-[1rem]">
                     {content}
